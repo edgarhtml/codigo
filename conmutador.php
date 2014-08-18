b7bec258027772ab278ccc6757fe2c41e9837184
edgarhtml <edgar.html@gmail.com>
2014-08-18 15:37:49 -0500

    conmutdor

---
+++ conmutador.php
@@ -0,0 +1,24 @@
+<?php>
+	$pais="FRANCIA";
+	switch ($pais){
+		case 'INGLATERRA':
+			echo("el usuarioes ingles.");
+			break;
+		case 'ALEMANIA':
+			echo ("el usuario es aleman");
+			break;
+		case 'ITALIA':
+			echo ("el usuario es italiano");
+			break;
+		case 'ESPAÑA':
+			echo ("el usuario es español");
+			break;
+		case 'FRANCIA':
+			echo ("el usuario es frances");
+			break;
+
+		default:
+			echo ("El usuario es de una nacionalidad no identificada");
+			break;
+		}
+		
