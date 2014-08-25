63951d32916110fd7be3e13c1997cdd794b2ea22
Hans <hans.php@gmail.com>
2014-08-25 16:01:00 -0500

    Dos maneras de incluir variables de POST

--- formularios/dos_form.php
+++ formularios/dos_form.php
@@ -1,7 +1,7 @@
 <?php
 	echo "El nombre de usuario es: $nombre <br>";
-	echo "¿El usuario es soltero?: $soltero <br>";
-	echo "El rango de edad de usuario es: $_post.cadena.edad.cadena <br>";
+	echo "¿El usuario es soltero?: $_POST[soltero] <br>";
+	echo "El rango de edad de usuario es: ".$_POST['edad']." <br>";
 	echo "La fecha es: $fecha <br>";
 	echo "El dato oculto escrito es: $datoOculto <br>";
 	echo "La contrasena es: $contraseña";
