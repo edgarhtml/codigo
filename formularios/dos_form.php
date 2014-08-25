c74c1ee8f0b24416e0c81b3678998fb5ef6eb28d
edgarhtml <edgar.html@gmail.com>
2014-08-25 15:56:49 -0500

    cambi1

--- formularios/dos_form.php
+++ formularios/dos_form.php
@@ -1,7 +1,7 @@
 <?php
 	echo "El nombre de usuario es: $nombre <br>";
 	echo "¿El usuario es soltero?: $soltero <br>";
-	echo "El rango de edad de usuario es: $edad <br>";
+	echo "El rango de edad de usuario es: $_post.cadena.edad.cadena <br>";
 	echo "La fecha es: $fecha <br>";
 	echo "El dato oculto escrito es: $datoOculto <br>";
 	echo "La contrasena es: $contraseña";
